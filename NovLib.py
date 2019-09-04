c96a038fe10b00b07112ae99886472809d3c6340
Alamar <54247703+AlamarW@users.noreply.github.com>
2019-09-03 22:08:41 -0400

    Update NovLib.py

--- NovLib.py
+++ NovLib.py
@@ -1,26 +1,37 @@
-
-# GUI Created by: PyQt5 UI code generator 5.13.0
+# Created By AlamarW
+# GUI Partially thank to: PyQt5 UI code generator 5.13.0
+# Text thanks to Gutenberg and their free library
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from nltk.corpus import gutenberg as gb
 from nltk.tokenize.treebank import TreebankWordTokenizer as TWT
 from nltk.tokenize.treebank import TreebankWordDetokenizer as  TWD
+from nltk.tokenize import sent_tokenize
     
-
 class text(object):
     #setting up the detokenizer function and the name|filename of the text
     t = TWT()
     d = TWD()
     libd_storage = []
+    raw_libd_text =[]
     
     textDict = {
-        "Alice in Wonderland" : "carroll-alice.txt",
-        "Moby Dick"           : "melville-moby_dick.txt",
-        "Leaves of Grass"     : "whitman-leaves.txt",
-        "King James Bible"    : "bible-kjv.txt",
-        "Hamlet"              : "shakespeare-hamlet.txt"}
-        
-        
+        "Alice in Wonderland"          : "carroll-alice.txt",
+        "Moby Dick"                    : "melville-moby_dick.txt",
+        "Leaves of Grass"              : "whitman-leaves.txt",
+        "King James Bible"             : "bible-kjv.txt",
+        "Hamlet"                       : "shakespeare-hamlet.txt",
+        "Emma"                         : "austen-emma.txt",
+        "Persuasion"                   : "austen-persuasion.txt",
+        "Sense and Sensibility"        : "austen-sense.txt",
+        "Poems of William Blake"       : "blake-poems.txt",
+        "The Wisdom of Father Brown"   : "chesterton-brown.txt",
+        "The Man Who Was Thursday"     : "chesterton-thursday.txt",
+        "Paradise Lost"                : "milton-paradise.txt",
+        "Julius Caesar"                : "shakespeare-caesar.txt",
+        "Macbeth"                      : "shakespeare-macbeth.txt"
+        }
+           
 class Ui_MainWindow(object):
 
     def setupUi(self, MainWindow):
@@ -49,18 +60,19 @@ class Ui_MainWindow(object):
         self.comboBox = QtWidgets.QComboBox(self.centralwidget)
         self.comboBox.setGeometry(QtCore.QRect(20, 60, 131, 21))
         self.comboBox.setObjectName("comboBox")
-        self.comboBox.addItems(text.textDict)
+        self.comboBox.addItems(sorted(text.textDict))
         
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setGeometry(QtCore.QRect(50, 40, 85, 16))
         self.label.setObjectName("label")
         
         self.radioButton = QtWidgets.QRadioButton(self.centralwidget)
-        self.radioButton.setGeometry(QtCore.QRect(20, 360, 140, 16))
+        self.radioButton.setGeometry(QtCore.QRect(20, 360, 163, 16))
         self.radioButton.setObjectName("radioButton")
+        self.radioButton.clicked.connect(self.all_libd_text)
         
         self.radioButton_2 = QtWidgets.QRadioButton(self.centralwidget)
-        self.radioButton_2.setGeometry(QtCore.QRect(20, 380, 195, 18))
+        self.radioButton_2.setGeometry(QtCore.QRect(20, 380, 200, 18))
         self.radioButton_2.setObjectName("radioButton_2")
         self.radioButton_2.clicked.connect(self.lib_sent_only)
         
@@ -113,7 +125,6 @@ class Ui_MainWindow(object):
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     
-
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "NovLibs"))
@@ -121,7 +132,7 @@ class Ui_MainWindow(object):
         self.impText.setText(_translate("MainWindow", "Import Text"))
         self.label.setText(_translate("MainWindow", "Novel To Lib"))
         self.radioButton.setText(_translate("MainWindow", "View Entire Text"))
-        self.radioButton_2.setText(_translate("MainWindow", "View Lib\'d Sentences only"))
+        self.radioButton_2.setText(_translate("MainWindow", "View Lib\'d Sentences Only"))
         self.label_2.setText(_translate("MainWindow", "Replace"))
         self.label_3.setText(_translate("MainWindow", "With"))
         self.label_4.setText(_translate("MainWindow", "Lib\'d Text"))
@@ -157,12 +168,20 @@ class Ui_MainWindow(object):
                 word_tokens[idx] = lib_word
 
         detokenized_words = text.d.detokenize(word_tokens)
-        self.textBrowser.append(detokenized_words)
         
+        #it's a surprise tool that will help us later: ref to all_libd_text method
+        text.raw_libd_text = detokenized_words 
+        sents = sent_tokenize(detokenized_words)
+        
+        for sentence in sents:
+            self.textBrowser.append(sentence)
+            self.textBrowser.append("\n")
+
     #"Clears" the libs, what it actually does is reload the text that represents what's in the combobox
     def clear_libs(self, MainWindow):
         self.textBrowser.clear()
         comboText = self.comboBox.currentText()
+        
         for i in text.textDict:
             if comboText == i:
                 rawText = gb.raw(text.textDict[i])
@@ -170,8 +189,33 @@ class Ui_MainWindow(object):
 
     #Uses the text.libd_storage to display only sentences with the the words in text.libd_storage
     def lib_sent_only(self, MainWindow):
-        print("good job")
-        pass
+        #print(text.libd_storage)
+        raw = self.textBrowser.toPlainText()
+        sents = sent_tokenize(raw)
+        self.textBrowser.clear()
+        temp_sentence_storage = []
+        
+        #maybe overly complicated but it does exactly what I needed it to do, somewhat
+        for sentence in sents:
+            words = text.t.tokenize(sentence)
+            for w in words:
+                for i in text.libd_storage:
+                    if i == w:
+                        temp_sentence_storage.append(sentence)
+                     
+        real_sentence_storage = list(dict.fromkeys(temp_sentence_storage))
+        
+        for i in real_sentence_storage:
+            self.textBrowser.append(i)
+            self.textBrowser.append("\n")
+
+    def all_libd_text(self, MainWindow):
+        self.textBrowser.clear()
+        changed_text = text.raw_libd_text
+        sents = sent_tokenize(changed_text)
+        for sentence in sents:
+            self.textBrowser.append(sentence)
+            self.textBrowser.append("\n")
                 
     #closes the gotdang application
     def close_application(self):
